9853a4e2d7e9a283b018ee014660e74fa28216a5
Ladislav Heller <ladislav.heller@gmail.com>
2017-04-09 20:24:05 +0200

    First chekin
    It works on my device.
    Need to check on other devices.
    Need to add some improvements

---
+++ autiovis/MainActivity.cs
@@ -0,0 +1,118 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Speech.Tts;
+using Android.Runtime;
+using Android.Content;
+using Android.Support.V4.View;
+using Android.Util;
+using Android.Content.PM;
+using Java.Util;
+
+namespace autiovis
+{
+	[Activity(Label = "@string/app_name",
+			  MainLauncher = true,
+			  Icon = "@mipmap/autism",
+			  Name = "hu.lheller.AutioVis",
+			  ScreenOrientation = ScreenOrientation.Portrait,
+			  Theme = "@android:style/Theme.Holo.Light.DarkActionBar"
+			 )]
+	public class MainActivity : Activity, TextToSpeech.IOnInitListener
+	{
+		private readonly string TAG = "[AutiOvis]";
+		private readonly string engine = "com.google.android.tts";
+		//private readonly string engine = "com.samsung.SMT";
+		private readonly string keyName = "cards";
+		private readonly int RC = 0x00050001;
+		private readonly int maxImages = 128;
+
+		public static TextToSpeech tts = null;
+
+		private VPAdapter vpa = null;
+		private ViewPager pager = null;
+
+		public void OnInit([GeneratedEnum] OperationResult status)
+		{
+			if (status == OperationResult.Error)
+			{
+				tts.SetLanguage(Locale.Default);
+			}
+		}
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.Main);
+
+			// configure TTS engine
+			tts = new TextToSpeech(this, this, engine);
+			var lang = Locale.Default;
+			tts.SetLanguage(lang);
+			tts.SetPitch(1.0f);
+			tts.SetSpeechRate(1.0f);
+
+			// configure viewpager
+			vpa = new VPAdapter(this, maxImages);
+			vpa.LoadList(keyName);
+			pager = FindViewById<ViewPager>(Resource.Id.pager);
+			pager.Adapter = vpa;
+
+			// configure PagerTabStrip
+			//var pts = pager.FindViewById<PagerTabStrip>(Resource.Id.pts);
+			//pts.SetBackgroundColor(Android.Graphics.Color.AliceBlue);
+
+			// get button and set click event handler
+			var btnAddPic = FindViewById<Button>(Resource.Id.btnAddPic);
+			btnAddPic.Click += (sender, e) =>
+			{
+				if (vpa.Count < maxImages)
+				{
+					var i = new Intent();
+					i.SetType("image/*");
+					i.SetAction(Intent.ActionGetContent);
+					StartActivityForResult(Intent.CreateChooser(i, "Pick an image..."), RC);
+				}
+				else
+				{
+					Toast.MakeText(this, "Maximum allowed images: " + maxImages, ToastLength.Short).Show();
+				}
+			};
+		}
+
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
+			tts.Shutdown();
+			vpa.SaveList(keyName);
+			Log.Debug(TAG, "Destroyed...");
+		}
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			base.OnActivityResult(requestCode, resultCode, data);
+			if ((requestCode == RC) && (resultCode == Result.Ok) && (data != null))
+			{
+				Log.Debug(TAG, "Media selected: " + data.DataString);
+				var uri = data.Data;
+
+				var txt = new EditText(this);
+				txt.Text = "Title";
+				var ad = new AlertDialog.Builder(this);
+				ad.SetTitle("Enter card title!");
+				ad.SetMessage("Entered text will appear below the new card.");
+				ad.SetView(txt);
+				ad.SetPositiveButton("OK", (sender, e) =>
+				{
+					vpa.AddCard(uri, txt.Text);
+					vpa.NotifyDataSetChanged();
+					pager.SetCurrentItem(vpa.Count - 1, true);
+				});
+				ad.SetNegativeButton("Cancel", (sender, e) => { });
+				ad.Show();
+			}
+		}
+	}
+}
+
---
+++ autiovis/vpAdapter.cs
@@ -0,0 +1,132 @@
+﻿using Android.Runtime;
+using Android.Content;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+using Android.Speech.Tts;
+using Android.Net;
+using Android.Support.V4.View;
+using System.Collections.Generic;
+using Java.Lang;
+using Com.Bumptech.Glide;
+using GoogleGson;
+using GoogleGson.Reflect;
+using Microsoft.CSharp;
+
+namespace autiovis
+{
+	public class VPAdapter : PagerAdapter
+	{
+		private readonly string TAG = "[AutiOvis]";
+		private Context mc = null;
+		private struct card
+		{
+			public Uri url;
+			public string desc;
+		}
+		private List<card> mCards = null;
+
+		// constructor
+		public VPAdapter(Context c, int max)
+		{
+			mc = c;
+			mCards = new List<card>(max);
+		}
+
+		// helper method - adds new item to the list
+		public void AddCard(Uri u, string d)
+		{
+			mCards.Add(new card() { url = u, desc = d });
+		}
+
+		// helper method - saves the whole list to as preference
+		public bool SaveList(string key)
+		{
+			if (mCards.Count == 0) return false;
+			var sp = mc.GetSharedPreferences("cards", FileCreationMode.Private);
+			var ed = sp.Edit();
+			var gson = new Gson();
+			var arr = new JsonArray();
+			foreach (card item in mCards) arr.Add(item.url.ToString() + "|" + item.desc);
+			var json = gson.ToJson(arr);
+			Log.Debug(TAG, json);
+			ed.Remove(key);
+			ed.PutString(key, json);
+			return ed.Commit();
+		}
+
+		// helper method - loads the list from preference
+		public void LoadList(string key)
+		{
+			var sp = mc.GetSharedPreferences("cards", FileCreationMode.Private);
+			var str = sp.GetString(key, "nothing");
+			if (str != "nothing")
+			{
+				var jp = new JsonParser();
+				var arr = jp.Parse(str).AsJsonArray;
+				Log.Debug(TAG, "Arr: " + arr.ToString());
+				Log.Debug(TAG, "Size: " + arr.Size());
+				for (int i = 0; i < arr.Size(); i++)
+				{
+					var item = arr.Get(i);
+					Log.Debug(TAG, "Item1: " + item.AsString);
+					var sc = item.AsString.Split(new char[] { '|' });
+					this.AddCard(Uri.Parse(sc[0]), sc[1]);
+				}
+			}
+		}
+
+		public override bool IsViewFromObject(View view, Object objectValue)
+		{
+			return view == objectValue;
+		}
+
+		public override int Count
+		{
+			get
+			{
+				return mCards.Count;
+			}
+		}
+
+		public override Object InstantiateItem(View container, int position)
+		{
+			var li = LayoutInflater.From(mc);
+			var ll = (LinearLayout)li.Inflate(Resource.Layout.Card, null, false);
+
+			var iv = ll.FindViewById<ImageView>(Resource.Id.img);
+			Glide.With(mc).LoadFromMediaStore(mCards[position].url).Thumbnail(0.1f).Into(iv);
+			iv.Click += (sender, e) =>
+			{
+				Log.Debug(TAG, "Clicked on position: " + position);
+				if (!MainActivity.tts.IsSpeaking) MainActivity.tts.Speak(mCards[position].desc, QueueMode.Flush, null);
+			};
+
+			var tv = ll.FindViewById<TextView>(Resource.Id.txt);
+			tv.Text = mCards[position].desc;
+			this.GetPageTitleFormatted(position);
+
+			var vp = container.JavaCast<ViewPager>();
+			vp.AddView(ll);
+			return ll;
+
+			//var ad = new Android.App.AlertDialog.Builder(mc);
+			//ad.SetTitle("Exception!");
+			//ad.SetMessage(ex.Message);
+			//ad.SetPositiveButton("OK", (sender, e) => { });
+			//ad.Show();
+			//Log.Error(TAG, ex.ToString());
+		}
+
+		public override void DestroyItem(View container, int position, Object objectValue)
+		{
+			var vp = container.JavaCast<ViewPager>();
+			vp.RemoveView(objectValue as View);
+		}
+
+		public override ICharSequence GetPageTitleFormatted(int position)
+		{
+			return new String(string.Format("[ {0} ]", mCards[position].desc));
+		}
+	}
+}
